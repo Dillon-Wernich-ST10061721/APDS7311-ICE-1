924dd6da18c76fe453d3eca5bda365e3bf65b8fe
Dillon Wernich <st10061721@vcconnect.edu.za>
2024-08-31 11:03:42 +0200

    Commt

---
+++ BACKEND/server.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+
+app.get('/',(req, res)=>{
+  res.send('I am finally figuring this out, no more crying')
+})
+
+app.get('/test', (req,res)=>{
+  res.send('Testing the /test endpoint')
+})
+
+// Start the Express server
+app.listen(PORT);
